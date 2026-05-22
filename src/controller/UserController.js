4ddd2ad685e812ac29dc2672b99fcc73354d6bae
GaureshMestry <mestrygauersh22@gmail.com>
2026-05-22 23:45:37 +0530

    Add controller for handling API requests

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -1,14 +1,103 @@
-const userService = require("../services/userService"); 
-const bcrypt = require("bcryptjs"); 
+const userService = require("../services/userService");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { use } = require("react");
 
 
 // REGISTER
- exports.register = async (req, res) => { 
-    try { 
-        await userService.registerUser(req.body); 
-   res.json({ message: "User Registered"  });  
-} catch (err) {  
-      res.status(500).json(err); 
-     
-    } };
+exports.register = async (req, res) => {
+    try {
+        await userService.registerUser(req.body);
+        res.json({ message: "User Registered" });
+    } catch (err) {
+        res.status(500).json(err);
+
+    }
+};
+
+
+
+//Login user
+exports.login = async (req, res) => {
+
+
+    try {
+        const user = await userService.loginUser(req.body.email);
+
+        if (!user) return res.status(400).json({ message: "User Not Found" })
+
+        const match = await bcrypt.compare(req.body.password, user.password)
+
+        if (!match) return res.status(400).json({ message: "Wrong Password" })
+
+        const token = jwt.sign(
+            {
+                id: user.user_id, role: user.role
+            },
+            "secret123",
+            {
+                expiresIn: "1d"
+            }
+        );
+
+        res.json({ token, user });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+
+//Get All User
+exports.getAllusers = async (req, res) => {
+
+    try {
+        const users = await userService.getAllUser();
+        res.json(users);
+    }
+    catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+
+
+
+//Get User By Id
+exports.getUserById = async (req, res) => {
+    try {
+        const user = await userService.getUserById(req.params.id);
+        res.json(user);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+
+
+
+//Update user
+exports.updateUser = (req, res) => {
+
+
+    try {
+        await userService.updateUser(req.params.id, req.body)
+        res.json({ messgae: "Updated" });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+}
+
+
+//Delete User
+
+exports.deleteUser=(req,res)=>{
+
+    try{
+        await userService.deleteUser(req.params.id)
+        res.json({
+           messgae: "successfully Deleted"
+        });
+    }catch(err){
+        res.status(500).json(err)
+    }
+}
